73a2f470433c3a15b090cec03067893ba2e450bd
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-01-30 17:38:23 -0500

    Updated from Brython Server: 1/30/2017 5:38:23 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,6 +1,6 @@
 """
 multiplication-table.py
-Author: <your name here>
+Author: <Brendan>
 Credit: <list sources used, if any>
 Assignment:
 
@@ -21,3 +21,11 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+
+w = input("What is the width of the multiplication table")
+l = input("What is the length of the multiplication table")
+
+for i in [range(1,int(w)+1)]:
+    for j in [range(1, int(l)+1)]:
+        print(i*j)
+    
