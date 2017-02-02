44c39536c1c288bada70b63e2b5aa251bd10288b
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-02 14:34:25 -0500

    Update multiplication-table.py

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <Brendan>
-Credit: <Ethan>
+Author: Brendan
+Credit: Ethan
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
