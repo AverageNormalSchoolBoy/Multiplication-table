932a9fb3fa73f2e777f1e7da7861ead5859c3993
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-02 19:53:30 -0500

    Update multiplication-table.py

--- multiplication-table.py
+++ multiplication-table.py
@@ -22,8 +22,8 @@ Height of multiplication table: 8
   8  16  24  32  40  48  56  64  72  80
 """
 
-l = input("Width of the multiplication table ")
-w = input("Length of the multiplication table ")
+l = input("Width of the multiplication table: ")
+w = input("Height of the multiplication table: ")
 a=1
 for i in list(range(1,int(w)+1)):
     print()
